d9f023fa652b12210289eb468c50b1da7917fec8
soso101 <109085716@qq.com>
2016-11-28 17:20:54 +0800

    update

--- avltree.h
+++ avltree.h
@@ -35,15 +35,17 @@ public:
 			return 0;
 	}
     static void updateHeight(AVLNode* node);
+
+	
     static AVLNode* llrotate(AVLNode* node);
     static AVLNode* rrrotate(AVLNode* node);
     static AVLNode* lrrotate(AVLNode* node);
     static AVLNode* rlrotate(AVLNode* node);
-	static AVLNode* avlNode2BSTNode(Node* node);
+	static AVLNode* BSTNode2AVLNode(Node* node);
 };
 
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
-typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::avlNode2BSTNode( Node* node )
+typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::BSTNode2AVLNode( Node* node )
 {
 	return static_cast<AVLNode*>(node);
 }
@@ -51,8 +53,8 @@ typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::avlNode2BSTNode( Nod
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 void AVLTree<T, Visitor>::updateHeight(AVLNode* node)
 {
-	AVLNode* left = static_cast<AVLNode*>(node->left);
-	AVLNode* right = static_cast<AVLNode*>(node->right);
+	AVLNode* left = BSTNode2AVLNode(node->left);
+	AVLNode* right = BSTNode2AVLNode(node->right);
 	if (!left && !right)
 		node->height = 1;
 	else
@@ -69,10 +71,17 @@ void AVLTree<T, Visitor>::insert( T key )
 	}
 }
 
+//              G                                  P
+//            /     \                             /      \
+//           P     Gr     =>            X        G
+//        /     \                             /          /    \
+//      X      Pr                         Xl       Pr    Gr
+//     / 
+//    Xl
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::llrotate( AVLNode* node )
 {
-    AVLNode* b = static_cast<AVLNode*>(node->left);
+    AVLNode* b = BSTNode2AVLNode(node->left);
 	node->left = b->right;
 	b->right = node;
 
@@ -81,10 +90,17 @@ typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::llrotate( AVLNode* n
     return b;
 }
 
+//         G                                      P
+//       /      \                                 /     \
+//     Gl     P          =>              G      X
+//              /   \                         /   \       \
+//            Pl   X                     Gl   Pl     Xr
+//                    \
+//                   Xr
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::rrrotate( AVLNode* node )
 {
-    AVLNode* b = static_cast<AVLNode*>(node->right);
+    AVLNode* b = BSTNode2AVLNode(node->right);
     node->right = b->left;
     b->left = node;
 
@@ -93,31 +109,86 @@ typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::rrrotate( AVLNode* n
     return b;
 }
 
+//          G                                   G                               X           
+//        /      \                               /    \                           /     \
+//       P     Gr                          X     Gr                     P      G
+//      /   \               =>             /   \                =>      /    \     /   \
+//     Pl   X                            P    Xr                     Pl   Xl  Xr  Gr
+//          /   \                          /   \
+//        Xl   Xr                      Pl  Xl
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::lrrotate( AVLNode* node )
 {
-    node->left = rrrotate(static_cast<AVLNode*>(node->left));
+    node->left = rrrotate(BSTNode2AVLNode(node->left));
     return llrotate(node);
 }
 
+//          G                               G                                       X
+//        /      \                          /     \                                   /     \
+//       Gl     P                      Gl     X                              G      P
+//               /   \        =>                /   \                 =>      /   \     /   \
+//             X    Pr                       Xl    P                      Gl   Xl  Xr  Pr 
+//           /   \                                      /   \
+//        Xl    Xr                                  Xr   Pr
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename AVLTree<T, Visitor>::AVLNode* AVLTree<T, Visitor>::rlrotate( AVLNode* node )
 {
-    node->right = llrotate(static_cast<AVLNode*>(node->right));
+    node->right = llrotate(BSTNode2AVLNode(node->right));
     return rrrotate(node);
 }
 
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename BSTree<T, Visitor>::Node* AVLTree<T, Visitor>::remove( Node*& node, Node* z )
 {
-	return NULL;
+	if (node == NULL || z == NULL)
+		return NULL;
+	if (node->key > z->key)
+	{
+		node->left = remove(node->left, z);
+		if (height(BSTNode2AVLNode(node->right)) - height(BSTNode2AVLNode(node->left)) == 2)
+		{
+			Node* n = node->right;
+			if (height(BSTNode2AVLNode(n->left)) > height(BSTNode2AVLNode(n->right)))
+				node = rlrotate(BSTNode2AVLNode(node));
+			else
+				node = rrrotate(BSTNode2AVLNode(node));
+		}
+	}
+	else if (node->key < z->key)
+	{
+		if (height(BSTNode2AVLNode(node->left)) - height(BSTNode2AVLNode(node->left)) == 2)
+		{
+			Node* n = node->left;
+			if (height(BSTNode2AVLNode(n->left)) > height(BSTNode2AVLNode(n->right)))
+				node = llrotate(BSTNode2AVLNode(node));
+			else
+				node = lrrotate(BSTNode2AVLNode(node));
+		}
+	}
+	else
+	{
+		if (node->left && node->right)
+		{
+			Node* n = maximum(node->left);
+			node->key = n->key;
+			node->left = remove(node->left, n);
+		}
+		else
+		{
+			Node* tmp = node;
+			node = node->left ? node->left : node->right;
+			delete tmp;
+		}
+	}
+
+	return node;
 }
 
 template<typename T, typename Visitor /*= BSTreeVisitor<T> */>
 typename BSTree<T, Visitor>::Node* AVLTree<T, Visitor>::insertNode( Node*& node, Node* z )
 {
-	AVLNode* x = static_cast<AVLNode* >(z);
-    AVLNode* n = static_cast<AVLNode* >(node);
+	AVLNode* x = BSTNode2AVLNode(z);
+    AVLNode* n = BSTNode2AVLNode(node);
 	if (!node)
 	{
         node = z;
@@ -126,25 +197,25 @@ typename BSTree<T, Visitor>::Node* AVLTree<T, Visitor>::insertNode( Node*& node,
     else if (z->key < node->key)
     {
         node->left = insertNode(node->left, z);
-        if (height(static_cast<AVLNode*>(node->left)) - height(static_cast<AVLNode*>(node->right)) == 2)
+        if (height(BSTNode2AVLNode(node->left)) - height(BSTNode2AVLNode(node->right)) == 2)
         {
             if (z->key < node->left->key)
                 node = llrotate(n);
             else
                 node = lrrotate(n);
-			n = static_cast<AVLNode* >(node);
+			n = BSTNode2AVLNode(node);
         }
     }
     else if (z->key > node->key)
     {
         node->right = insertNode(node->right, z);
-        if (height(static_cast<AVLNode*>(node->right)) - height(static_cast<AVLNode*>(node->left)) == 2)
+        if (height(BSTNode2AVLNode(node->right)) - height(BSTNode2AVLNode(node->left)) == 2)
         {
             if (z->key > node->right->key)
                 node = rrrotate(n);
             else
                 node = rlrotate(n);
-			n = static_cast<AVLNode* >(node);
+			n = BSTNode2AVLNode(node);
         }
     }
 
